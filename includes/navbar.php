a97cc31fdb4b83597ff2e42609a1f220d956d849
Zach <zachcopland@Zachs-MacBook-Pro.local>
2017-11-06 14:56:26 -0500

    fixed look

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,27 +7,21 @@ echo <<<HTML
         <div class="navbar-header">
           <a href="#" class="navbar-brand">What's My Shift</a>
         </div>
-        <div>
-          <ul class="nav navbar-nav">
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse navHeaderCollapse">
+          <ul class="nav navbar-nav navbar-right">
             <!-- Main Nav item -->
             <li class=""><a href="index.php">Home</a></li>
             <!-- Other Menu items -->
-            <li class="desktopNav"><a href="employer-info.php">Employer info</a></li>
-            <li class="desktopNav"><a href="#">FAQ</a></li>
-            <li class="desktopNav"><a href="terms-and-conditions.html">Terms</a></li>
-            <li class="desktopNav"><a href="versions.php">Versions</a></li>
-          <!-- Dropdown Menu -->
-            <li class="dropdown mobileNav">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Links <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <!-- Dropdown Menu items -->
-                <li class="mobileNav"><a href="employer-info.php">Employer info</a></li>
-                <li class="mobileNav"><a href="#">FAQ</a></li>
-                <li class="mobileNav"><a href="terms-and-conditions.html">Terms</a></li>
-                <li class="mobileNav"><a href="versions.php">Versions</a></li>
-                </ul> 
-                <!-- End of Dropdown Menu items -->
-            </li>
+            <li><a href="employer-info.php">Employer info</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="bug-reporter.php">Bug Reporter</a></li>
+            <li><a href="terms-and-conditions.html">Terms</a></li>
+            <li><a href="versions.php">Versions</a></li>
           </ul>
           <!-- End of Main Nav items -->
         </div>
