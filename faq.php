c716bd75cafd58d023f62dd6517a27779b7eef91
Zach <zachcopland@Zachs-MacBook-Pro.local>
2017-11-10 08:36:15 -0500

    initial commit

---
+++ faq.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+  <head>
+    <title>FAQ</title>
+    <?php include 'includes/header.php'; ?>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
+      <?php include 'includes/navbar.php'; ?>
+    <div class="container">
+        <h1 class="vermillion-color">Frequently Asked Questions</h1><hr><br/>
+        <div class="row">
+            <h4><b>How do I sign up?</b></h4>
+            <h5>It is easy for administrators and employees to sign up! Take a look at either the administrator or employee tutorial videos.</h5>
+        </div>
+        <div class="row">
+            <h4><b>How do I reset my password?</b></h4>
+            <h5>If you are an employee, contact your administrator and they will allow you to reset your password. After they have reset your password, go to the home page and click "I forgot my password" below the login screen. If you are an administrator, please send an email <a href="mailto:zcopland16@gmail.com?subject=WhatsMyShift Admin Password Reset">here</a> to reset your password.</h5>
+        </div>
+        <div class="row">
+            <h4><b>How will I know when my schedule changes?</b></h4>
+            <h5>Your administrator has the ability to text or email you once they make changes to your schedule or if they have any announcements to make.</h5>
+        </div>
+        <div class="row">
+            <h4><b>Does it cost any money to sign up as an employee?</b></h4>
+            <h5>Signing up as an employee is 100% free.</h5>
+        </div>
+        <div class="row">
+            <h4><b>How do I know my Company ID?</b></h4>
+            <h5>When signing up, your administrator should tell you what the Company ID is. If they have forgotten, they can login and view their admin panel and see what it is.</h5>
+        </div>
+        <div class="row">
+            <h4><b>How old do my employees have to be to sign up?</b></h4>
+            <h5>To use this service, all users must be 16 years or older, per our <a href="terms-and-conditions.html">Terms and Conditions</a>.</h5>
+        </div>
+    </div>
+    <script type="text/javascript"></script>
+    <script>
+        $(document).ready(function() {
+            
+        });
+    </script>
+  </body>
+</html>
