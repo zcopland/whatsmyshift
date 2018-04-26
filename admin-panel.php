9afca935933b3c450bcc5210ca329f67aa1666fb
Zach <zachcopland@Zachs-MacBook-Pro.local>
2018-04-25 20:30:31 -0400

    added favicon

--- admin-panel.php
+++ admin-panel.php
@@ -54,6 +54,9 @@ echo <<<HTML
   <head>
     <title>Admin Panel</title>
     <meta charset='utf-8' />
+HTML;
+    include 'includes/header.php';
+echo <<<HTML
 	<!-- Start of Bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
