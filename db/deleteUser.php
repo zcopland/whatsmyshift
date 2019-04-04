063b64d9d8d3e061f0864b13289330d4f99ad40b
Zach <zcopland16@gmail.com>
2019-04-03 22:09:16 -0400

    add deleted user to a table just in case of error by admin

--- db/deleteUser.php
+++ db/deleteUser.php
@@ -4,17 +4,24 @@ include 'dbh_root.php';
 $val = $_POST['val'];
 $companyID = $_POST['companyID'];
 
-$sql = "DELETE FROM `employees` WHERE id = {$val};";
+$sql = "INSERT INTO `employees_deleted` SELECT * FROM `employees` WHERE employees.id = {$val};";
 $result = mysqli_query($conn_root, $sql);
 
+
 if ($result) {
-    $sql = "SELECT * FROM `companies` WHERE companyID='{$companyID}';";
+    $sql = "DELETE FROM `employees` WHERE id = {$val};";
     $result = mysqli_query($conn_root, $sql);
-    $row = mysqli_fetch_assoc($result);
-    $count = $row['organizationCount'] - 1;
-    $sql = "UPDATE `companies` SET organizationCount={$count} WHERE companyID='{$companyID}';";
-    $result2 = mysqli_query($conn_root, $sql);
-    echo true;
+    if ($result) {
+        $sql = "SELECT * FROM `companies` WHERE companyID='{$companyID}';";
+        $result = mysqli_query($conn_root, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $count = $row['organizationCount'] - 1;
+        $sql = "UPDATE `companies` SET organizationCount={$count} WHERE companyID='{$companyID}';";
+        $result2 = mysqli_query($conn_root, $sql);
+        echo true;
+    } else {
+        echo $sql;
+    }
 } else {
     echo $sql;
 }
